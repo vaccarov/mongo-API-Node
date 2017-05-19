64bc68d17ed7c30e0f4a6cd1501a292237a5f0cd
Victor Vaccaro <victor.vaccaro@ibm.com>
2017-05-19 16:15:59 +0200

    test deploy db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://victor:victor@ds147711.mlab.com:47711/todos' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 
+// POST new Todo
 app.post('/todos', (req, res) => {
   console.log(req.body);
   var todo = new Todo({
     text: req.body.text
   });
-
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
@@ -22,6 +24,32 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.get('/todos', (req, res) => {
+  Todo.find().then((docs) => {
+    res.send({docs});
+  });
 });
+
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('ID INVALIDE');
+  }
+
+  Todo.findById(id).then((match) => {
+    if (!match) {
+      return res.send('ID NON TROUVE');
+    }
+    res.send({match});
+  }).catch((e) => {
+    return res.status(400).send();
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
+
+module.exports = {app};
